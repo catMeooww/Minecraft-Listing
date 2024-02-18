6bfbc6f9d37389c12b800b2a13b0cd01bd1bb5b7
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2024-02-17 22:40:24 -0300

    Add files via upload

--- ListJoing.js
+++ ListJoing.js
@@ -1,21 +1,29 @@
 function addList() {
-    listName = document.getElementById("list-name").value;
+  listName = document.getElementById("list-name").value;
+  if (!listName == "") {
     sm = "Started a new list: " + listName
     firebase.database().ref("/").child(listName).update({
       servermessage: sm
     });
     localStorage.setItem("list", listName);
     redirect();
+  } else {
+    document.getElementById("log-list-output").innerHTML = "List name can not be Empty";
   }
+}
 
-  function joinList(){
-    listName = document.getElementById("list-name").value;
+function joinList() {
+  listName = document.getElementById("list-name").value;
+  if (!listName == "") {
     localStorage.setItem("list", listName);
     redirect();
+  } else {
+    document.getElementById("log-list-output").innerHTML = "Can not log-in a list without name";
   }
+}
 
-  function redirect(){
-    setTimeout(()=>{
-      window.location = "list.html";
-    },500)
-  }
+function redirect() {
+  setTimeout(() => {
+    window.location = "list.html";
+  }, 500)
+}
