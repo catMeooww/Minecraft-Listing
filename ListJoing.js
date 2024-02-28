6d0522f8ac957d2d87dcc527d54d79150601cbcd
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2024-02-27 22:10:05 -0300

    Log-in Update

--- ListJoing.js
+++ ListJoing.js
@@ -1,22 +1,26 @@
 function addList() {
-  listName = document.getElementById("list-name").value;
-  if (!listName == "") {
-    sm = "Started a new list: " + listName
-    firebase.database().ref("/").child(listName).update({
-      servermessage: sm
-    });
-    localStorage.setItem("list", listName);
-    redirect();
-  } else {
-    document.getElementById("log-list-output").innerHTML = "List name can not be Empty";
-  }
+  window.location = "list-creating.html";
 }
 
 function joinList() {
   listName = document.getElementById("list-name").value;
   if (!listName == "") {
-    localStorage.setItem("list", listName);
-    redirect();
+    var listref = firebase.database().ref(listName + "/servermessage");
+    var isListCreated;
+    var isJoining = false;
+    listref.on("value", data => {
+      isListCreated = data.val();
+      console.log(isListCreated);
+      if (!isJoining) {
+        isJoining = true;
+        if (isListCreated == "Started a new list: " + listName) {
+          localStorage.setItem("list", listName);
+          redirect();
+        } else {
+          document.getElementById("log-list-output").innerHTML = "Could not find list";
+        }
+      }
+    });
   } else {
     document.getElementById("log-list-output").innerHTML = "Can not log-in a list without name";
   }
